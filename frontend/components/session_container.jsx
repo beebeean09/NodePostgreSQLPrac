18f34f443648b101ccbfd511f84cccc4f94f6973
beebeean09 <beebeean09@gmail.com>
2017-07-02 18:22:58 -0700

    add session actions to container

--- frontend/components/session_container.jsx
+++ frontend/components/session_container.jsx
@@ -1,12 +1,22 @@
 import { connect } from 'react-redux';
 import Session from './session';
+import { signup, logout, login, clearErrors } from '../actions/session_actions';
 
-const mapStateToProps = (state) => ({
-  currentUser: Boolean(state.session)
+const mapStateToProps = (state, ownProps) => ({
+  currentUser: Boolean(state.session),
+  errors: state.session.errors,
+  ownProps: ownProps,
+  propType: ownProps.pathname.location.slice(1) == 'signup' ? 'Sign Up' : 'Login'
 });
 
-// const mapDispatchToProps = (dispatch) => ({
-//
-// });
+const mapDispatchToProps = (dispatch, ownProps) => {
+  let processForm = ownProps.location.pathname.slice(1) === 'login' ? login : signup;
 
-export default connect(mapStateToProps, null)(Session);
+  return {
+    processForm: (user) => dispatch(processForm(user)),
+    logout: () => dispatch(logout()),
+    clearErrors: () => dispatch(clearErrors())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Session);
